790828d0f3b74fd51d11499d31b671f17bb2307b
zhangtingyu11 <18013933973@163.com>
2021-08-31 22:37:13 +0800

    增加路侧数据

--- pointcloud_utils/coor_trans.py
+++ pointcloud_utils/coor_trans.py
@@ -32,6 +32,30 @@ def lidar_coor2gps(car_lon, car_lat, car_heading, obj_x, obj_y, obj_angle):
     return obj_lon, obj_lat, obj_heading
 
 
+def lidar_coor2gps_road(car_lon, car_lat, car_heading, obj_x, obj_y, obj_angle):
+    """[将激光雷达坐标系下的障碍物转换到gps坐标]
+
+    Args:
+        car_lon ([type]): [主车的经度]
+        car_lat ([type]): [主车的纬度]
+        car_heading ([type]): [主车的航向角， 与正北方向夹角，逆时针为正]
+        obj_x ([type]): [障碍物在lidar坐标系下的x坐标]
+        obj_y ([type]): [障碍物在lidar坐标系下的y坐标]
+        obj_angle ([type]): [障碍物和主车航向的夹角，顺时针为正]
+    """
+    # 计算障碍物距离车辆后轴中心点的距离
+    distance = math.sqrt(
+        math.pow(obj_x, 2) + math.pow(obj_y, 2))
+    # 障碍物和车辆后轴中心点的连线和车头方向的夹角（0-360，逆时针为正）
+    obj2car_angle = math.degrees(math.atan2(obj_y, obj_x))
+    # 获取障碍物的航向角（与正北方向夹角，顺时针为正）
+    azm = 360 - ((obj2car_angle + car_heading) % 360)
+    obj_lon, obj_lat, _ = geod.fwd(car_lon, car_lat, azm, distance)
+    obj_heading = ((car_heading - obj_angle) % 360)
+    # 返回的航向角和正北方夹角，逆时针为正
+    return obj_lon, obj_lat, obj_heading
+
+
 def gps2lidar_coor(car_lon, car_lat, car_heading, obj_lon, obj_lat, obj_heading):
     """[将障碍物的经纬度转化为激光雷达坐标系的坐标]
 
@@ -47,6 +71,26 @@ def gps2lidar_coor(car_lon, car_lat, car_heading, obj_lon, obj_lat, obj_heading)
     # 障碍物和主车的夹角，顺时针为正，0-360
     obj2car_angle = (forward_amz + car_heading) % 360
     obj_x = distance * math.cos(math.radians(obj2car_angle)) - IMU_2_LIDAR_X_OFFSET
+    obj_y = distance * math.sin(math.radians(obj2car_angle))
+    obj_angle = (car_heading - obj_heading) % 360
+    return obj_x, -obj_y, obj_angle
+
+
+def gps2lidar_coor_road(car_lon, car_lat, car_heading, obj_lon, obj_lat, obj_heading):
+    """[将障碍物的经纬度转化为激光雷达坐标系的坐标]
+
+    Args:
+        car_lon ([type]): [主车经度]
+        car_lat ([type]): [主车纬度]
+        car_heading ([type]): [主车航向角， 与正北方向夹角，逆时针为正]
+        obj_lon ([type]): [障碍物经度]
+        obj_lat ([type]): [障碍物纬度]
+        obj_heading ([type]): [障碍物航向角，和正北方夹角，逆时针为正]
+    """
+    forward_amz, _, distance = geod.inv(car_lon, car_lat, obj_lon, obj_lat)
+    # 障碍物和主车的夹角，顺时针为正，0-360
+    obj2car_angle = (forward_amz + car_heading) % 360
+    obj_x = distance * math.cos(math.radians(obj2car_angle))
     obj_y = distance * math.sin(math.radians(obj2car_angle))
     obj_angle = (car_heading - obj_heading) % 360
     return obj_x, -obj_y, obj_angle
--- pointcloud_utils/coor_trans_demo.py
+++ pointcloud_utils/coor_trans_demo.py
@@ -5,6 +5,8 @@ import open3d as o3d
 
 pcd_map = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/map/map.pcd"
 pcd_20m = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/20m/20m.pcd"
+pcd_road = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/map/map.pcd"
+
 
 file_20m = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/20m/20m.txt"
 file_40m = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/40m/40m.txt"
@@ -21,8 +23,10 @@ car_map_lon = 125.1569996
 car_map_lat = 43.8332673
 car_map_heading = 200.94
 
-road_lon = 125.1570688216
-road_lat = 43.8329770885
+road_lon = 125.15707001863602
+road_lat = 43.832977952695146
+road_heading = 295.3
+
 
 file_20m_gps = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/20m/20m_gps.txt"
 file_40m_gps = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/40m/40m_gps.txt"
@@ -32,7 +36,8 @@ file_20m_in_map = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/20m/20m_
 file_40m_in_map = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/40m/40m_in_map.txt"
 file_road_in_map = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/road/road_in_map.txt"
 
-file_road_gps = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/road/obj.txt"
+file_road = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/road/road_in_road.txt"
+file_road_gps = "/home/zty/My_Project/pointcloud_utils/data/pcd_files/road/road_gps.txt"
 
 f_20m = open(file_20m, "r+")
 f_20m_lines = f_20m.readlines()
@@ -94,12 +99,26 @@ f_40m_in_map.writelines(f_40m_gps_lines)
 f_40m_in_map.close()
 
 
+f_road = open(file_road, "r+")
+f_road_lines = f_road.readlines()
+for i in range(len(f_road_lines)):
+    classification, occlusion, length, width, height, x, y, z, theta, angle, score = f_road_lines[
+        i].rstrip().split('\t')
+    obj_lon, obj_lat, obj_heading = coor_trans.lidar_coor2gps_road(
+        road_lon, road_lat, road_heading, float(x), float(y),
+        float(angle))
+    f_road_lines[i] = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+        classification, occlusion, length, width, height, obj_lon, obj_lat, z,
+        theta, obj_heading, score)
+f_road_gps = open(file_road_gps, 'w+')
+f_road_gps.writelines(f_road_lines)
+
 f_road_gps = open(file_road_gps, "r+")
 f_road_gps_lines = f_road_gps.readlines()
 for i in range(len(f_road_gps_lines)):
     classification, occlusion, length, width, height, lon, lat, z, theta, heading, score = f_road_gps_lines[
         i].rstrip().split('\t')
-    obj_x, obj_y, obj_angle = coor_trans.gps2lidar_coor(
+    obj_x, obj_y, obj_angle = coor_trans.gps2lidar_coor_road(
         car_map_lon, car_map_lat, car_map_heading, float(lon), float(lat),
         float(heading))
     f_road_gps_lines[i] = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
@@ -109,7 +128,8 @@ f_road_in_map = open(file_road_in_map, 'w+')
 f_road_in_map.writelines(f_road_gps_lines)
 f_road_in_map.close()
 
-pc = PointCloud(channel_num=4, filename=pcd_map)
+
+pc = PointCloud(channel_num=4, filename=pcd_road)
 pc.create_vis()
 # # 显示路侧数据
 # obj_x, obj_y, obj_angle = coor_trans.gps2lidar_coor(car_map_lon, car_map_lat,
@@ -127,7 +147,7 @@ pc.create_vis()
 # line_set.lines = o3d.utility.Vector2iVector(lines_box)
 # line_set.colors = o3d.utility.Vector3dVector(colors)
 # pc.vis.add_geometry(line_set)
-pred_boxes = pc.draw_3dboxes_from_txt(file_map, [1, 1, 1])
+# pred_boxes = pc.draw_3dboxes_from_txt(file_map, [1, 1, 1])
 road_boxrs = pc.draw_3dboxes_from_txt(file_road_in_map, [1, 0, 0])
 
 
